23ca4eb3ffb87640e77ca9f07b0f7dc3acbcf7a5
Safvan <safvanparangodath12@gmail.com>
2020-11-24 00:36:39 +0530

    My first commit

--- src/index.js
+++ src/index.js
@@ -1,25 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import Ajax from './Ajax';
-//import Form from './Form';
 import App from './App';
-//import Counter from './Counter';
-//import reportWebVitals from './reportWebVitals';
-//import {Header} from "./Home";
-//import Counternew from './Counternew';
-//import Demo from './Demo';
-//import Demonew from './Demonew';
-
-
-//var x = setInterval( () => {
-//    ReactDOM.render(<Counter/>,document.getElementById('root'));
-//},1000)
 ReactDOM.render(<App/>,document.getElementById('root'));
-
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
